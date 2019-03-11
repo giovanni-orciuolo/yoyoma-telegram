4a59cd85583584348ab51c2efea73797ac0caab1
Giovanni Orciuolo <giovanni.orciuolo1999@gmail.com>
2019-03-11 18:09:27 +0100

    Improved i18n Still implementing speechToText (not working yet) "dev" command works properly now with nodemon

--- src/speechToText.js
+++ src/speechToText.js
@@ -3,6 +3,24 @@ const fs = require('fs')
 const https = require('https')
 const WITAI_TOKEN = process.env.WITAI_TOKEN
 
+const TG_FILE_URL = "https://api.telegram.org/file"
+
+const downloadTelegramAudio = (audio, dest) => {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(dest)
+    https.get(`${TG_FILE_URL}/bot${process.env.BOT_TOKEN}/${audio.file_path}`, res => {
+      res.pipe(file)
+      file.on('finish', () => {
+        file.close()
+        resolve()
+      })
+    }).on('error', err => {
+      fs.unlink(dest)
+      reject(err.message)
+    })
+  })
+}
+
 const extractSpeech = (stream, contentType) => {
   return new Promise((resolve, reject) => {
     witSpeech.extractSpeechIntent(WITAI_TOKEN, stream, contentType, {}, (err, res) => {
@@ -14,29 +32,30 @@ const extractSpeech = (stream, contentType) => {
 
 const speechToText = async (ctx) => {
   if (!ctx.message.voice) return
+  console.log('Trying to parse that...')
+  await ctx.telegram.sendChatAction(ctx.chat.id, 'typing')
+
+  const voiceFile = await ctx.telegram.getFile(ctx.message.voice.file_id)
+  const voicePath = `audio/${voiceFile.file_id}.mp3`
+
   try {
-    console.log('Trying to parse that...')
-    await ctx.telegram.sendChatAction(ctx.chat.id, 'typing')
+    await downloadTelegramAudio(voiceFile, voicePath)
+  } catch (err) {
+    console.error('Error while parsing speech to text!', err)
+    ctx.reply(ctx.i18n.t('s2t__download_fail'), {
+      reply_to_message_id: ctx.message.message_id
+    })
+  }
 
-    const voice = await ctx.telegram.getFile(ctx.message.voice.file_id)
-    console.log(voice)
-
-    const voicePath = `audio/${voice.file_id}.mp3`
-    const voiceFile = fs.createWriteStream(voicePath)
-    await new Promise(resolve =>
-      https.get(`https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${voice.file_path}`, res => {
-        res.pipe(voiceFile)
-        resolve(res)
-      })
-    )
+  try {
     const voiceStream = fs.createReadStream(voicePath)
-
-    console.log(voiceStream)
-    const parsedSpeech = await extractSpeech(voiceStream, 'audio/mp3')
+    const parsedSpeech = await extractSpeech(voiceStream, ctx.message.voice.mime_type)
     console.log(parsedSpeech)
   } catch (err) {
     console.error('Error while parsing speech to text!', err)
-    ctx.reply(ctx.i18n.t('s2t__fail'))
+    ctx.reply(ctx.i18n.t('s2t__transcribe_fail'), {
+      reply_to_message_id: ctx.message.message_id
+    })
   }
 }
 module.exports = {
