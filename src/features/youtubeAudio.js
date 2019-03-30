bebbe1d2989845db3a7d2db83dc949a0afe9d400
Giovanni Orciuolo <giovanni.orciuolo1999@gmail.com>
2019-03-30 20:12:38 +0100

    Added a one hour limit to videos downloaded through /ytaudio
    Version bump 0.5.4

--- src/features/youtubeAudio.js
+++ src/features/youtubeAudio.js
@@ -1,19 +1,32 @@
 const search = require('youtube-search')
+const info = require('youtube-info')
 const ytAudioStream = require('youtube-audio-stream')
 const { generate } = require('shortid')
 const fs = require('fs')
 
+const MAX_VIDEO_LENGTH = 3600 // seconds
+
+const youtubeInfo = (id) => {
+  return new Promise((resolve, reject) => {
+    if (!id) {
+      reject({ status: 400, message: "ID must have a value" })
+    }
+    info(id)
+      .then((videoInfo) => resolve(videoInfo))
+      .catch((err) => reject(err))
+  })
+}
+
 const youtubeSearch = (terms) => {
   return new Promise((resolve, reject) => {
     if (!terms) {
-      reject({ status: 404, message: "Empty search" })
+      reject({ status: 404, message: "Empty search..." })
     }
     search(terms, {
       maxResults: 1,
       key: process.env.GOOGLE_API_TOKEN
     }, (err, results) => {
       if (err) reject(err)
-      console.log(terms)
       resolve(results)
     })
   })
@@ -22,10 +35,15 @@ const youtubeSearch = (terms) => {
 const downloadYoutubeAudio = (searchTerms) => {
   return new Promise((resolve, reject) => {
     youtubeSearch(searchTerms)
-      .then((results) => {
+      .then(async (results) => {
         const result = results[0]
         if (!result)
           reject({ status: 404, message: "No video found on Youtube!" })
+
+        const videoInfo = await youtubeInfo(result.id)
+        if (videoInfo.duration > MAX_VIDEO_LENGTH) {
+          reject({ status: 413, message: "Video is too long!" })
+        }
 
         const dest = `./audio/yt_${result.id}_${generate()}.mp3`
         const writeStream = fs.createWriteStream(dest)
@@ -33,7 +51,7 @@ const downloadYoutubeAudio = (searchTerms) => {
         ytAudioStream(result.link).pipe(writeStream)
         writeStream.on('finish', () => {
           writeStream.close()
-          resolve({ path: dest, title: result.title})
+          resolve({ path: dest, title: result.title, meta: videoInfo })
         })
       })
       .catch(err => reject(err))
@@ -46,12 +64,13 @@ const sendYoutubeAudio = async (ctx) => {
     await ctx.replyWithAudio({ source: data.path }, {
       title: data.title
     })
-    fs.unlink(data.path, (err) => {})
+    fs.unlink(data.path, () => {})
   } catch (err) {
     console.error("Error while sending youtube audio!", err)
     let what = 'ytaudio__error'
     switch (err.status) {
       case 404: what = 'ytaudio__not_found'; break
+      case 413: what = 'ytaudio__too_long'; break
     }
     ctx.reply(ctx.i18n.t(what), {
       reply_to_message_id: ctx.message.message_id,
@@ -61,6 +80,5 @@ const sendYoutubeAudio = async (ctx) => {
 }
 
 module.exports = {
-  youtubeSearch,
   sendYoutubeAudio
 }
